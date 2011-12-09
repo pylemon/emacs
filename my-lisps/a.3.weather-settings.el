d68b17917627049c523ea04d27ed4c47b455ac86
pylemon <liwei2@ata.net.cn>
2011-12-09 15:05:01 +0800

    emacs config start

---
+++ my-lisps/a.3.weather-settings.el
@@ -0,0 +1,22 @@
+;; 功能:
+;; 使用命令在minibuffer中查询当前设置的城市的天气情况
+;; 
+;; 键盘绑定:
+;; "C-c C-w" cn-weather
+;;
+;; 修改时间:
+;; 2011年12月9日
+
+
+(require 'cn-weather)
+
+;; 设置命令别名
+(defalias 'weather 'cn-weather-today)
+(defalias 'weather-tomorrow 'cn-weather-forecast)
+
+;; 设置需要查询的城市
+(setq cn-weather-city "上海")
+(global-set-key (kbd "C-c C-w") 'cn-weather)
+
+;; 启动时读取一次，下次查询时不会出现nil
+(cn-weather)
