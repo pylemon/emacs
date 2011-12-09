d6110f0102543e928561c893a7382ef65c69ab79
pylemon <liwei2@ata.net.cn>
2011-12-09 15:55:04 +0800

    remove kbd-used.org

--- my-lisps/z.9.kbd-settings.el
+++ my-lisps/z.9.kbd-settings.el
@@ -1,5 +1,15 @@
 ;; 功能: 设置所有键盘绑定相关的东东
 
-
-; 查询当前天气
+;; 查询天气
 (global-set-key (kbd "C-c C-w") 'cn-weather)
+
+;; 标记 Mark set
+(global-unset-key (kbd "C-SPC"))
+(global-set-key (kbd "s-SPC") 'set-mark-command)
+
+;; ibus 输入法切换
+(global-set-key (kbd "C-SPC") 'ibus-toggle)
+
+;; 切换 buffer 向前向后
+(global-set-key (kbd "<C-tab>") 'wcy-switch-buffer-forward)
+(global-set-key (kbd "C-`") 'wcy-switch-buffer-backward)
