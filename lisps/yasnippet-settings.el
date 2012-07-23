6042f9ac8bee3a514ab8e8c618119657f385dce5
pylemon <leeway1985@gmail.com>
2012-07-23 17:17:39 +0800

    move all yasnippet settings to yasnippet-settings.el

--- lisps/yasnippet-settings.el
+++ lisps/yasnippet-settings.el
@@ -2,45 +2,57 @@
 
 ;; Time-stamp: <2010-04-26 10:22:22 Monday by ahei>
 
+(require 'auto-complete)
 (require 'yasnippet)
 
-(setq yas/root-directory "~/emacs/lisps/snippets")
-(yas/load-directory yas/root-directory)
+;; 自动补全的配置
+(add-to-list 'ac-dictionary-directories "~/emacs/auto-complete/ac-dict")
+(global-auto-complete-mode t)
+(setq-default ac-sources '(ac-source-words-in-same-mode-buffers))
+(add-hook 'emacs-lisp-mode-hook (lambda () (add-to-list 'ac-sources 'ac-source-symbols)))
+(add-hook 'auto-complete-mode-hook (lambda () (add-to-list 'ac-sources 'ac-source-filename)))
 
+(setq ac-auto-start 2)
+(setq ac-dwim t)
+(define-globalized-minor-mode real-global-auto-complete-mode
+  auto-complete-mode (lambda ()
+                       (if (not (minibufferp (current-buffer)))
+                           (auto-complete-mode 1))
+                       ))
+(real-global-auto-complete-mode t)
+
+(define-key ac-completing-map "\M-n" 'ac-next)
+(define-key ac-completing-map "\M-p" 'ac-previous)
+
+(setq yas/root-directory "~/emacs/snippets")
+(yas/load-directory yas/root-directory)
 (yas/global-mode 1)
 
-;; (defun yasnippet-settings ()
-;;   "settings for `yasnippet'."
-;;   (setq yas/root-directory (concat "~/emacs/lisps/" "snippets"))
+(defun ac-yasnippet-candidate ()
+  (let ((table (yas/get-snippet-tables major-mode)))
+    (if table
+      (let (candidates (list))
+            (mapcar (lambda (mode)
+              (maphash (lambda (key value)
+                (push key candidates))
+              (yas/snippet-table-hash mode)))
+            table)
+        (all-completions ac-prefix candidates)))))
 
-;;   (defun yasnippet-unbind-trigger-key ()
-;;     "Unbind `yas/trigger-key'."
-;;     (let ((key yas/trigger-key))
-;;       (setq yas/trigger-key nil)
-;;       (yas/trigger-key-reload key)))
+(defface ac-yasnippet-candidate-face
+  '((t (:background "sandybrown" :foreground "black")))
+  "Face for yasnippet candidate.")
 
-;;   (yasnippet-unbind-trigger-key)
-  
-;; ;;;###autoload
-;;   (defun yasnippet-reload-after-save ()
-;;     (let* ((bfn (expand-file-name (buffer-file-name)))
-;;            (root (expand-file-name yas/root-directory)))
-;;       (when (string-match (concat "^" root) bfn)
-;;         (yas/load-snippet-buffer))))
-;;   (add-hook 'after-save-hook 'yasnippet-reload-after-save))
+(defface ac-yasnippet-selection-face
+  '((t (:background "coral3" :foreground "white")))
+  "Face for the yasnippet selected candidate.")
 
-;; (eal-define-keys
-;;  'yas/keymap
-;;  `(("M-j"     yas/next-field-or-maybe-expand)
-;;    ("M-p"     yas/prev-field)))
-
-;; (eal-define-keys
-;;  'yas/minor-mode-map
-;;  `(("C-c C-f" yas/find-snippets)))
-
-;; (eval-after-load "yasnippet"
-;;   `(yasnippet-settings))
-
-;; (yas/load-directory yas/root-directory)
+(defvar ac-source-yasnippet
+  '((candidates . ac-yasnippet-candidate)
+    (action . yas/expand)
+    (limit . 3)
+    (candidate-face . ac-yasnippet-candidate-face)
+    (selection-face . ac-yasnippet-selection-face))
+  "Source for Yasnippet.")
 
 (provide 'yasnippet-settings)
